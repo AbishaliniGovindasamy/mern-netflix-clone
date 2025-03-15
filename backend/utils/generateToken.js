d5d39c7894bfb4347071e9f522b5f27f5a6ca50d
Abishalini1602 <abiabishalini1002@gmail.com>
2025-03-15 10:47:22 +0530

    Updated generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,22 +2,19 @@ import jwt from "jsonwebtoken";
 import { ENV_VARS } from "../config/envVars.js";
 
 export const generateTokenAndSetCookie = (userId, res) => {
-   
+    // ✅ Generate JWT Token
+    const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" });
 
-        // ✅ Generate JWT Token
-        const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" });
+    // ✅ Set Cookie with Token
+    res.cookie("jwt-netflix", token, {
+        httpOnly: true,  // ✅ Prevents XSS attacks
+        secure: ENV_VARS.NODE_ENV !== "development", // ✅ Secure in production
+        sameSite: "Strict", // ✅ Prevents CSRF
+        maxAge: 15 * 24 * 60 * 60 * 1000 // ✅ 15 Days Expiration
+    });
 
-        // ✅ Set Cookie with Token
-        res.cookie("jwt-netflix", token, {
-            httpOnly: true,  // ✅ Prevents XSS attacks
-            secure: ENV_VARS.NODE_ENV =! "development", // ✅ Secure in production
-            sameSite: "strict", // ✅ Prevents CSRF
-            maxAge: 15 * 24 * 60 * 60 * 1000 // ✅ 1 Day Expiration
-        });
-
-        // ✅ Debugging Logs
-        console.log("✅ Token generated and cookie set successfully!");
-        console.log("🔑 Token:", token); // ⚠️ Remove this in production
-return token;
-   
+    // ✅ Debugging Logs
+    console.log("✅ Token generated and cookie set successfully!");
+    console.log("🔑 Token:", token); // ⚠️ Remove this in production
+    return token;
 };
